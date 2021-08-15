3111f08f9e265a2541b4d3cb883141f7c2bba336
Aakash <aakash@LAPTOP-QS7689OR.localdomain>
2021-08-15 13:08:44 +0530

    initial

---
+++ q6_dijkstra.java
@@ -0,0 +1,82 @@
+import java.util.*;
+class Pair{
+    int v,cost;
+    Pair(int v,int cost){
+        this.v =v ;
+        this.cost = cost;
+    }
+}
+class q6_dijkstra {
+    Map<Integer,ArrayList<Pair>> m = new HashMap<Integer,ArrayList<Pair>>();
+    
+    public void solve(int v, int e,int src) {
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>(v,(p1,p2)->p1.cost<=p2.cost?-1:1);
+        Map<Integer,Boolean> visited = new HashMap<>();        
+        int[] dis= new int[v+1];
+
+        for(int i = 0 ;i<v;i++){
+            dis[i] = Integer.MAX_VALUE;
+        }
+        dis[src] = 0;
+        
+        pq.add(new Pair(src,0));
+        while(pq.size()!=0){
+            Pair p = pq.poll();
+            if(visited.get(p.v)!=null){
+                continue;
+            }
+            visited.put(p.v,true);
+            dis[p.v] = p.cost;
+
+            for(Pair pc : m.get(p.v)){
+                pq.add(new Pair(pc.v,pc.cost+p.cost));
+            }
+        }
+        for(int i = 1 ; i <=v;i++){
+            System.out.println(i +" : "+dis[i] );
+        }
+        
+    }
+    q6_dijkstra(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of vertices ");
+        int v = scan.nextInt();
+        System.out.println("Enter number of edges");
+        int e = scan.nextInt();
+
+        for(int i =0  ; i<e;i++){
+            System.out.printf("enter edge %d in the format vertex1 vertex2 cost\n",i+1);
+            int v1 = scan.nextInt();
+            int v2 = scan.nextInt();
+            int cost = scan.nextInt();
+
+            ArrayList<Pair> v1edges = m.get(v1);
+            if(v1edges==null)v1edges = new ArrayList<Pair>();
+            ArrayList<Pair> v2edges = m.get(v2);
+            if(v2edges == null)v2edges = new ArrayList<Pair>();
+
+            v1edges.add(new Pair(v2,cost));
+            v2edges.add(new Pair(v1,cost));
+            m.put(v1, v1edges);
+            m.put(v2, v2edges);
+        }
+        solve(v,e,1);
+    }
+
+    public static void main(String[] args) {
+        new q6_dijkstra();
+
+    }
+}
+
+
+//Input
+
+// 5
+// 6
+// 1 2 10 
+// 1 5 100
+// 2 3 50
+// 5 3 10
+// 5 4 60
+// 3 4 20
---
+++ q7_mst.java
@@ -0,0 +1,78 @@
+import java.util.*;
+class pair{
+    int v,cost,parent;
+    pair(int v,int cost,int parent){
+        this.v =v ;
+        this.cost = cost;
+        this.parent = parent;
+    }
+}
+
+class q7_mst {
+    Map<Integer,ArrayList<pair>> m = new HashMap<Integer,ArrayList<pair>>();
+    
+    public void solve(int v, int e,int src) {
+        PriorityQueue<pair> pq = new PriorityQueue<pair>(v,(p1,p2)->p1.cost<=p2.cost?-1:1);
+        Map<Integer,Boolean> visited = new HashMap<>();        
+        
+        ArrayList<pair> edges = new ArrayList<pair>();
+        
+        pq.add(new pair(src,0,-1));
+        while(pq.size()!=0){
+            pair p = pq.poll();
+            if(visited.get(p.v)!=null){
+                continue;
+            }
+            visited.put(p.v,true);
+            if(p.parent!=-1)
+                edges.add(new pair(p.v,p.cost,p.parent));
+            for(pair pc : m.get(p.v)){
+                pq.add(new pair(pc.v,pc.cost+p.cost,p.v));
+            }
+        }
+        for(int i = 0 ; i <edges.size();i++){
+            System.out.println(edges.get(i).v + " - " + edges.get(i).parent  );
+        }
+        
+    }
+    q7_mst(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of vertices ");
+        int v = scan.nextInt();
+        System.out.println("Enter number of edges");
+        int e = scan.nextInt();
+
+        for(int i =0  ; i<e;i++){
+            System.out.printf("enter edge %d in the format vertex1 vertex2 cost\n",i+1);
+            int v1 = scan.nextInt();
+            int v2 = scan.nextInt();
+            int cost = scan.nextInt();
+
+            ArrayList<pair> v1edges = m.get(v1);
+            if(v1edges==null)v1edges = new ArrayList<pair>();
+            ArrayList<pair> v2edges = m.get(v2);
+            if(v2edges == null)v2edges = new ArrayList<pair>();
+
+            v1edges.add(new pair(v2,cost,-1));
+            v2edges.add(new pair(v1,cost,-1));
+            m.put(v1, v1edges);
+            m.put(v2, v2edges);
+        }
+        solve(v,e,1);
+    }
+
+    public static void main(String[] args) {
+        new q7_mst();
+
+    }
+}
+//input
+// 5
+// 7
+// 5 1 5
+// 5 4 3
+// 3 4 2
+// 3 2 1
+// 2 1 10
+// 4 2 6
+// 5 3 7
